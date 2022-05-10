8fba06a095639e57735057cdf5e69c117641e82c
Harsh <harsh.sen@arnasoftech.com>
2022-05-10 16:23:49 +0530

    Commit

--- FINAL/ClientApp/src/components/Home.js
+++ FINAL/ClientApp/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { CategoryData } from '../service/Service';
-
+import '../../public/assets/css/style.css'
 export class Home extends Component {
   constructor(props) {
       super(props);
@@ -38,24 +38,128 @@ export class Home extends Component {
         console.log(SuperCategoryList);
     return (
         <>
-            <div className="container py-md-4 mt-lg-0 mt-mb-4">
-                <div className="grids-area-hny main-cont-wthree-fea row justify-content-center">
+            <div >
+                {/*<div className="grids-area-hny main-cont-wthree-fea row justify-content-center">*/}
                   
-                    {SuperCategoryList.map(items =>
-                        <div className="card mb-4" >
-                            {/*<img className="card-img-top" style={{ maxWidth: '100px' }} src="../../Content/img/12.png" alt="" />*/}
-                            <h1>
-                                <i class={items.logo}></i>
-                            </h1>
-                            <div className="card-body">
-                                <h5 className="card-title text-center">{items.catName}</h5>
+                {/*    {SuperCategoryList.map(items =>*/}
+                {/*        <div className="card mb-4" >*/}
+                {/*            */}{/*<img className="card-img-top" style={{ maxWidth: '100px' }} src="../../Content/img/12.png" alt="" />*/}
+                {/*            <h1>*/}
+                {/*                <i class={items.logo}></i>*/}
+                {/*            </h1>*/}
+                {/*            <div className="card-body">*/}
+                {/*                <h5 className="card-title text-center">{items.catName}</h5>*/}
 
-                            </div>
-                            <NavLink to={"/category/" + items.catId} className="overlay-link"></NavLink>
+                {/*            </div>*/}
+                {/*            <NavLink to={"/category/" + items.catId} className="overlay-link"></NavLink>*/}
                            
-                        </div>
-                    )}
-                </div>
+                {/*        </div>*/}
+                {/*    )}*/}
+                {/*</div>*/}
+
+
+                {/*<div class="modal fade" id="useItWalletPopUp" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">*/}
+                {/*    <div class="modal-dialog modal-dialog-centered" style={{width: "30%;"}} role="document">*/}
+                {/*        <div class="modal-content rounded shadow border-0">*/}
+                {/*            <div class="modal-header">*/}
+                {/*                <h5 class="modal-title" id="exampleModalCenterTitle">Cart Details</h5>*/}
+                {/*                <button type="button" class="close" data-dismiss="modal" aria-label="Close">*/}
+                {/*                    <span aria-hidden="true">&times;</span>*/}
+                {/*                </button>*/}
+                {/*            </div>*/}
+                {/*            <div class="modal-body">*/}
+                {/*                <input type="hidden" id="isWalletUse" value="0"/>*/}
+                {/*                    <div class="bg-white p-2 rounded box-shadow" id="CartdynamicUseWalletDiv">*/}
+                {/*                    </div>*/}
+                {/*            </div>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+
+            
+             
+
+                    <section class="main-slider mainSection" id="home">
+                        <ul class="slides">
+                        <li class="bg-slider slider-rtl-2 d-flex align-items-center" style={{background:"url('https://www.papertax.in/assets/images/paper_tax_images/home-slider1.png') center center;"}}>
+                                <div class="container">
+                                    <div class="row align-items-center mt-5">
+                                        <div class="col-lg-7 col-md-7">
+                                            <div class="title-heading mt-4">
+                                                <h1 class="display-4 title-white font-weight-bold mb-3">Let's file tax <span class="text-primary text-decoration-underline typewrite" data-period="2000" data-type='[ "easily", "easily", "easily", "easily" ]'> <span class="wrap"></span> </span> </h1>
+                                                <p class="para-desc text-muted para-dark">Now manage your taxes & business legal compliances with India's first e-commerce platform offering legal services, just choose from the 200+ services and get going..</p>
+                                                <div class="mt-4">
+                                                <a href={"https://www.papertax.in/products"} class="btn btn-soft-primary">Shop Now</a>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        <li class="bg-slider slider-rtl-1 d-flex align-items-center" style={{ background: "url('https://www.papertax.in/assets/images/paper_tax_images/home-slider2.jpg') center center; background-repeat: no-repeat;" }}>
+                                <div class="container">
+                                    <div class="row align-items-center mt-5">
+                                        <div class="col-lg-7 col-md-7">
+                                            <div class="title-heading mt-4">
+                                                <h1 class="display-4 title-white font-weight-bold mb-3">Sit, Relax & <span class="text-primary text-decoration-underline typewrite" data-period="2000" data-type='[ "have a good time!", "have a good time!", "have a good time!", "have a good time!" ]'> <span class="wrap"></span> </span> </h1>
+                                                <p class="para-desc text-muted para-dark">You are a taxpayer, it's your right to sit, relax and have a coffee till the time we complete your task.</p>
+                                                <div className="mt-4">
+                                                <a href={"https://www.papertax.in/products"} className="btn btn-soft-primary">Shop Now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        <li className="bg-slider slider-rtl-3 d-flex align-items-center" style={{ background: "url('https://www.papertax.in/assets/images/paper_tax_images/home-slider3.jpg') center center; background-repeat: no-repeat;" }}>
+                                <div className="container">
+                                    <div className="row align-items-center mt-5">
+                                        <div className="col-lg-7 col-md-7">
+                                            <div className="title-heading mt-4">
+                                            <h1 className="display-4 title-white font-weight-bold mb-3">Tax on <span className="text-primary text-decoration-underline typewrite" data-period="2000" data-type='[ "Fingertips...", "Fingertips...", "Fingertips...", "Fingertips..." ]'> <span className="wrap"></span> </span> </h1>
+                                                    <p className="para-desc text-muted para-dark">Now don’t worry of managing various login for your family, relatives and friends legal compliances, you can manage all in single app with paper tax.</p>
+                                                <div className="mt-4">
+                                                <a href={"https://www.papertax.in/products"} className="btn btn-soft-primary">Shop Now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        <li className="bg-slider slider-rtl-4 d-flex align-items-center" style={{ background: "url('https://www.papertax.in/assets/images/paper_tax_images/home-slider4.jpg') center center; background-repeat: no-repeat;" }}>
+                                <div className="container">
+                                    <div className="row align-items-center mt-5">
+                                        <div className="col-lg-7 col-md-7">
+                                            <div className="title-heading mt-4">
+                                                <h1 className="display-4 title-white font-weight-bold mb-3">Tax on <span className="text-primary text-decoration-underline typewrite" data-period="2000" data-type='[ "Fingertips...", "Fingertips...", "Fingertips...", "Fingertips..." ]'> <span className="wrap"></span> </span> </h1>
+                                                <p className="para-desc text-muted para-dark">With the exclusive website and mobile app loaded with 200+ legal services, just fill your cart with what all you need and you are all set.</p>
+                                                <div className="mt-4">
+                                                <a href={"https://www.papertax.in/products"} className="btn btn-soft-primary">Shop Now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        <li className="bg-slider slider-rtl-5 d-flex align-items-center" style={{ background: "url('https://www.papertax.in/assets/images/paper_tax_images/home-slider5.jpg') center center; background-repeat: no-repeat;" }}>
+                                <div className="container">
+                                    <div className="row align-items-center mt-5">
+                                        <div className="col-lg-7 col-md-7">
+                                            <div className="title-heading mt-4">
+                                                <h1 className="display-4 title-white font-weight-bold mb-3">You are in a <span className="text-primary text-decoration-underline typewrite" data-period="2000" data-type='[ "good company!", "good company!", "good company!", "good company!" ]'> <span className="wrap"></span> </span> </h1>
+                                                <p className="para-desc text-muted para-dark">Lacs of tax returns filed till date with almost 98% accuracy qualifies us to process more than INR 15 crore tax refund for our clients and the good news is “numbers are increasing daily".</p>
+                                                <div className="mt-4">
+                                                <a href={"https://www.papertax.in/products"} className="btn btn-soft-primary">Shop Now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                </section>
+
+
             </div>
 
         </>
--- FINAL/ClientApp/src/components/Layout.js
+++ FINAL/ClientApp/src/components/Layout.js
@@ -7,118 +7,99 @@ export class Layout extends Component {
         debugger;
         e.preventDefault();
         var element = document.body;
-        element.classList.toggle("dark-mode");
+        element.classNameList.toggle("dark-mode");
     }
     render() {
         return (
 
-            <div>
+            <div id="MainBody">
+                <div id="loader" classNameName="center text-center-loader" style={{ display: "none"}}>
+                    <img src="https://www.papertax.in/assets/images/spinner.gif" alt="loader"/>
+                </div>
 
-             
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <a class="navbar-brand" href="#">
-                        <img src="assets/img/logo.png" alt="Dhar Info"/>
-                    </a>
-                        <button class="navbar-toggler" type="button" data-trigger="#main_nav">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="navbar-collapse" id="main_nav">
-                            <div class="offcanvas-header mt-3">
-                                <button type="button" class="btn-close float-right"><i class="fas fa-times"></i></button>
-                                <h6 class="py-2 text-white"><i class="fas fa-phone-square mr-2"></i>+91 999 38 111 22</h6>
+                    <div id="preloader">
+                        <div id="status">
+                            <div className="spinner">
+                                <div className="double-bounce1"></div>
+                                <div className="double-bounce2"></div>
                             </div>
-                            <ul class="navbar-nav">
-                                <li class="nav-item active"> <a class="nav-link" href="#"><i class="fas fa-home mr-2 listing"></i>Home </a> </li>
-                                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-bus mr-2 listing"></i> Bus Infomation </a></li>
-                                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-users mr-2 listing"></i> Social Group</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#"> <i class="fas fa-phone mr-2 listing"></i>Important Contact</a></li>
-                          
-                            </ul>
-                            <div class="dropdown-divider"></div>
-                            <ul class="navbar-nav ml-auto">
-                            <li class="nav-item"><a id="openmodel" class="nav-link" data-toggle="modal" data-target="#RegModel"><i class="fas fa-user mr-2 listing"></i>Add Listing </a></li>
-                            <button onClick={this.doSomething}>Dark Mode</button>
-                            </ul>
-                        </div> 
-                    </nav>
-                    <div class="search-bar mt-md-4">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-10 mx-auto">
-                                    <h2 class="searchtitle">Search Anything Near You!</h2>
-                                    <div class="p-1 bg-light shadow-sm">
-                                        <div class="input-group">
-                                            <input type="search" placeholder="search anythig here" class="form-control border-0 bg-light" />
-                                                <div class="input-group-append">
-                                                    <div class="btn-group">
-                                                        <button type="button" class="btn btn-outline-secondary dropdown-toggle smallcreeen" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                            Business Directory
-                                                        </button>
-                                                        <div class="dropdown-menu">
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-briefcase mr-2" aria-hidden="true"></i>Business</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i>Education</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-medkit mr-2" aria-hidden="true"></i>Health & Medical</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-car mr-2" aria-hidden="true"></i>Automobiles</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-calendar mr-2" aria-hidden="true"></i>Events & Functions</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-building mr-2" aria-hidden="true"></i>Offices</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-wrench mr-2" aria-hidden="true"></i>Machines & Tools</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-bus mr-2" aria-hidden="true"></i>Transport & Travels</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-users mr-2" aria-hidden="true"></i>Service Providers</a>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-city mr-2" aria-hidden="true"></i>Building & Construction</a>
-                                                            <div class="dropdown-divider"></div>
-                                                            <a class="dropdown-item" href="#"><i class="fa fa-align-justify mr-2" aria-hidden="true"></i>All Category</a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="input-group-append">
-                                                        <button type="submit" class="btn btn-link"><i class="fa fa-search" aria-hidden="true"></i></button>
-                                                    </div>
-                                                </div>
-                                        </div>
-                                    </div>
-                                </div>
+                        </div>
+                    </div>
+
+
+                    <header id="topnav" className="defaultscroll sticky">
+                        <div className="container">
+                            <div>
+                            <a className="logo" href={"https://www.papertax.in/"}>
+                                <img style={{ maxWidth: "100%", height: "75px" }} src="https://www.papertax.in/assets/images/paper_tax_images/paan-legal-logo.svg" alt="papertax logo" />
+                                </a>
                             </div>
-                    </div>
-                    </div>
-
-
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="../../Content/img/s1.jpg" alt="First slide" />
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="../../Content/img/s2.jpg" alt="Second slide" />
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="../../Content/img/s3.jpg" alt="Third slide" />
-                        </div>
-                    </div>
-               </div>               
-                                    
-                                    <div class="modal fade" id="RegModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Registration</h5>
-                                                    <button id="closemdl" type="button" class="" data-dismiss="modal" aria-label=" "> <span aria-hidden="true">&times;</span> </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div id="svg_wrap">
-                                                    </div>
-                                                </div>
+                                <ul className="buy-button list-inline mb-0">
+                                    <li className="list-inline-item mb-0">
+                                        <div className="dropdown dropdown-primary">
+                                    <button type="button" className="btn btn-primary dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="uil uil-user align-middle icons"></i><i className="uil uil-angle-down align-middle icons"></i></button>
+                                    <div className="dropdown-menu dropdown-menu-right bg-white shadow rounded border-0 mt-3 py-3" style={{ width: "200px" }}>
+                                                <a className="dropdown-item text-dark" href="https://www.papertax.in/register"><i className="uil uil-user-plus align-middle mr-1"></i>Sign up</a>
+                                                <div className="dropdown-divider my-3 border-top"></div>
+                                                <a href="https://www.papertax.in/login" className="dropdown-item text-dark"><i className="uil uil-sign-in-alt align-middle mr-1"></i>Login</a>
                                             </div>
                                         </div>
+                                    </li>
+                                    <li className="list-inline-item mb-0 pr-1">
+                                        <div className="dropdown">
+                                            <button type="button" className="btn  btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="uil uil-shopping-cart align-middle icons"></i></button>
+                                    <a id="cartItemCountDiv" href={"https://www.papertax.in/get-cart-details"}></a>
+                                    <div className="dropdown-menu dropdown-menu-right bg-white shadow rounded border-0 mt-3 p-4" id="dynamicCartData" style={{ width: "300px;" }}>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+
+                                <div className="menu-extras">
+                                    <div className="menu-item">
+
+                                        <a className="navbar-toggle">
+                                            <div className="lines">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                            </div>
+                                        </a>
+
                                     </div>
+                                </div>
+                                <div id="navigation">
+
+                                    <ul className="navigation-menu">
+                                        <li className="home"><a href="https://www.papertax.in/#home" className="h6 text-dark">Home</a></li>
+                                        <li className="incomeTax"><a href="https://www.papertax.in/income-tax#incomeTax" className="h6 text-dark">Income Tax</a></li>
+                                        <li className="WhyUs"><a href="https://www.papertax.in/gst" className="h6 text-dark">GST</a>
+                                        </li>
+                                        <li className="has-submenu">
+                                            <a href="javascript:void(0)">Explore</a><span className="menu-arrow"></span>
+                                            <ul className="submenu">
+                                                <li><a href="https://www.papertax.in/app-showcase">App Showcase</a></li>
+                                                <li><a href="https://www.papertax.in/paper-tax-wallet">Paper Tax Wallet</a></li>
+                                                <li><a href="https://www.papertax.in/faqs">FAQ's</a></li>
+                                                <li><a href="https://www.papertax.in/contact-us">Contact us</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <div className="buy-menu-btn d-none">
+                                        <a href="https://www.papertax.in/login" className="btn btn-pills btn-soft-light mt-2 mr-2">Login</a>
+                                    </div>
+                                    <div className="buy-menu-btn d-none">
+                                        <a href="https://www.papertax.in/register" className="btn btn-pills btn-danger mt-2 mr-2">Sign up</a>
+                                    </div>
+                        </div>
+                        </div>
+                         </header>
 
                     <Container>
                         {this.props.children}
                         </Container>
-           </div>
+            </div>
+
                    
                 );
   }
